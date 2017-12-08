c268b7a7cb29a7de45054ea27117eb5101c360bf
Caleb Rose <clrose86@gmail.com>
2017-12-08 16:23:03 -0600

    added html

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+// start button begins the game
+
+// show one question at a time. each question has an independant timer
+
+// when user chooses an answer or when timer runs out show the correct answer 
+// and let user know if timer ran out or if answer was correct or incorrect.
+
+// after all questions are finished, show user number of correct, incorrect, and unanswered questions. 
+
+// add option to retake quiz that does not reload the page
